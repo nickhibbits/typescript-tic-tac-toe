090a01af7dbeee2e42c2924072772ce904a92603
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-09-19 22:01:02 -0700

    add win conditions

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ var boardState = [
     ["", "", ""],
 ];
 var currentMove = "X";
+var winner = "";
 function createCell(row, col, content) {
     if (content === void 0) { content = ""; }
     var cell = document.createElement("button");
@@ -16,13 +17,79 @@ function createCell(row, col, content) {
     cell.setAttribute("data-content", content);
     cell.classList.add("cell");
     cell.addEventListener("click", function () {
+        if (winner)
+            return;
         if (boardState[row][col] === "") {
             boardState[row][col] = currentMove;
             currentMove = "X" ? "O" : "X";
+            winner = checkBoard();
             renderBoard();
         }
     });
     return cell;
+}
+var victories = [
+    [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+    ],
+    [
+        [1, 0],
+        [1, 1],
+        [1, 2],
+    ],
+    [
+        [2, 0],
+        [2, 1],
+        [2, 2],
+    ],
+    [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+    ],
+    [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+    ],
+    [
+        [0, 2],
+        [1, 2],
+        [2, 2],
+    ],
+    [
+        [0, 0],
+        [1, 1],
+        [2, 2],
+    ],
+    [
+        [0, 2],
+        [1, 1],
+        [2, 0],
+    ],
+];
+function checkBoard() {
+    for (var _i = 0, victories_1 = victories; _i < victories_1.length; _i++) {
+        var victory = victories_1[_i];
+        var cell1 = boardState[victory[0][0]][victory[0][1]];
+        var cell2 = boardState[victory[1][0]][victory[1][1]];
+        var cell3 = boardState[victory[2][0]][victory[2][1]];
+        if (cell1 !== "" && cell1 === cell2 && cell2 === cell3) {
+            return cell1;
+        }
+    }
+    var isDraw = true;
+    for (var i = 0; i < ROW_COUNT; i++) {
+        for (var j = 0; j < COL_COUNT; j++) {
+            if (boardState[i][j] === "")
+                isDraw = false;
+        }
+    }
+    if (isDraw)
+        "Draw";
+    return "";
 }
 function renderBoard() {
     if (!appElement)
